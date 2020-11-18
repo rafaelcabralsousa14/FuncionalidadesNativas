9f7b49962374ad98edce818ec47f788475760616
rafaelcabralsousa14 <61585019+rafaelcabralsousa14@users.noreply.github.com>
2020-11-18 09:43:34 -0300

    Camera completa

--- App.js
+++ App.js
@@ -1,29 +1,22 @@
 import 'react-native-gesture-handler';
 import * as React from 'react';
-import { NavigationContainer, TabActions } from '@react-navigation/native';
+import { NavigationContainer, TabActions, Tab } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Contatos from './pages/Contatos';
 import TextToSpeech from './pages/TextToSpeech'
 
 const App = () => {
+
   return (
     <NavigationContainer>
       <Tab.Navigator>
         <Tab.Screen name="Contato" component={Contatos} />
         <Tab.Screen name="TextToSpeech" component={TextToSpeech} />
         <Tab.Screen name="Location" component={Location} />
+        <Tab.Screen name="Camera" component={Camera} />
       </Tab.Navigator>
     </NavigationContainer>
   );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+};
 
 export default App;
